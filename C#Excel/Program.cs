2b394d407b76c9f063061fe6bed7e93e215e1292
dersmith <dersmith@OCN-L-64T4YY2>
2025-01-15 13:51:04 -0500

    Add project files.

---
+++ C#Excel/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace C_Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Excel File");
+
+            string filePath = Console.ReadLine();
+            if (filePath == string.Empty) { filePath = "sample.xlsx"; }
+            if (!File.Exists(filePath))
+            {
+                using (SpreadsheetDocument doc = SpreadsheetDocument.Create(filePath, SpreadsheetDocumentType.Workbook))
+                {
+                    WorkbookPart workbookPart = doc.AddWorkbookPart();
+                    workbookPart.Workbook = new Workbook();
+                    WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                    worksheetPart.Worksheet = new Worksheet(new SheetData());
+                    Sheets sheets = doc.WorkbookPart.Workbook.AppendChild(new Sheets());
+                    Sheet sheet = new Sheet() {
+                        Id = doc.WorkbookPart.GetIdOfPart(worksheetPart), 
+                        SheetId = 1, 
+                        Name = "Sheet1" 
+                    };
+                    sheets.Append(sheet);
+                    workbookPart.Workbook.Save();
+                }
+            }
+
+            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, false))
+            {
+                WorkbookPart workbookPart = doc.WorkbookPart;
+                Sheet sheet = workbookPart.Workbook.Descendants<Sheet>().First();
+                WorksheetPart worksheetPart = (WorksheetPart)workbookPart.GetPartById(sheet.Id);
+                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+
+                if (!sheetData.Elements<Row>().Any())
+                {
+                    Console.WriteLine("Current Excel file is empty");
+                }
+                else
+                {
+                    Console.WriteLine("This is the current content of your file:");
+                    foreach (Row row in sheetData.Elements<Row>())
+                    {
+                        foreach (Cell cell in row.Elements<Cell>())
+                        {
+                            Console.Write(cell.CellValue.Text + "\t");
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("\nWould you like to add a new row?");
+            string response = Console.ReadLine();
+
+        }
+    }
+}
